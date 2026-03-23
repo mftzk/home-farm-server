6b1deb0821d7fa17adaa2c9160fca2a935ee9086
Aji Mufti <ajizkai1301@gmail.com>
2026-03-23 19:16:34 +0700

    update dockerfile

--- database/migrations/2024_01_01_000000_create_light_readings_table.php
+++ database/migrations/2024_01_01_000000_create_light_readings_table.php
@@ -8,6 +8,10 @@ return new class extends Migration
 {
     public function up(): void
     {
+        if (Schema::hasTable('light_readings')) {
+            return;
+        }
+
         Schema::create('light_readings', function (Blueprint $table) {
             $table->increments('id');
             $table->float('lux');
